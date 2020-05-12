40c09ff5e4e13e5f985d26de862fbfe06b49b252
mikeoconnor <1531855+mikeoconnor@users.noreply.github.com>
2020-05-12 23:48:21 +0100

    Rename "bool public isRunning" to "bool private running" in Stoppable

--- contracts/Stoppable.sol
+++ contracts/Stoppable.sol
@@ -3,31 +3,35 @@ pragma solidity 0.5.0;
 import "./Ownable.sol";
 
 contract Stoppable is Ownable {
-    bool public isRunning = true;
+    bool private running = true;
 
     event LogStopped(address sender);
     event LogResumed(address sender);
 
     modifier ifRunning {
-        require(isRunning);
+        require(running, "contract not running");
         _;
     }
 
     modifier ifStopped {
-        require(!isRunning);
+        require(!running, "contract not stopped");
         _;
     }
 
     function stop() public onlyOwner ifRunning returns(bool success){
-        isRunning = false;
+        running = false;
         emit LogStopped(msg.sender);
         return true;
     }
 
     function resume() public onlyOwner ifStopped returns(bool success){
-        isRunning = true;
+        running = true;
         emit LogResumed(msg.sender);
         return true;
+    }
+
+    function isRunning() public view returns(bool){
+        return running;
     }
 }
 
