c960134d5f3108801b35a48c1666693efba6af48
mikeoconnor <1531855+mikeoconnor@users.noreply.github.com>
2020-05-15 13:56:45 +0100

    Update unit test to check expiration date
    Use npm package ganache-time-traveler to advance time in the unit tests.

--- test/remittance.test.js
+++ test/remittance.test.js
@@ -1,4 +1,5 @@
 const Remittance = artifacts.require("Remittance");
+const tm = require('ganache-time-traveler');
 
 let instance = null;
 let tx = null;
@@ -7,6 +8,12 @@ const secret1 = "onetime1";
 const secret2 = "onetime2";
 const text = "onetime1";
 const text2 = "onetime2";
+
+// The expiration date
+const Tue_15_Sep_00_00_00_BST_2020 = 1600124400;
+
+// Ten days after expiration date
+const Fri_25_Sep_00_00_00_BST_2020 = 1600988400;
 
 contract('Remittance - Given new contract', (accounts) => {
     const owner = accounts[0]
@@ -19,7 +26,7 @@ contract('Remittance - Given new contract', (accounts) => {
 
     it('should allow alice to set puzzle with funds', async () => {
         puzzle = await instance.generatePuzzle(secret1, secret2);
-        tx = await instance.setupPuzzleAndFunds(puzzle, {from: alice, value: web3.utils.toWei('2', 'ether')});
+        tx = await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020, {from: alice, value: web3.utils.toWei('2', 'ether')});
         const { logs } = tx;
         const log = logs[0];
         assert.equal(log.event, 'LogSetup');
@@ -49,12 +56,12 @@ contract('Remittance -Given puzzle set by alice', (accounts) => {
     beforeEach('set up contract with puzzle and funds from alice', async () => {
         instance = await Remittance.new({from: owner});
         puzzle = await instance.generatePuzzle(secret1, secret2);
-        await instance.setupPuzzleAndFunds(puzzle, {from: alice, value: web3.utils.toWei('2', 'ether')});
+        await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020, {from: alice, value: web3.utils.toWei('2', 'ether')});
     });
 
     it('Should not allow alice to reset puzzle', async () => {
         try{
-            await instance.setupPuzzleAndFunds(puzzle, {from: alice, value: web3.utils.toWei('2', 'ether')});
+            await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020, {from: alice, value: web3.utils.toWei('2', 'ether')});
             throw null;
         } catch(error) {
             assert.isNotNull(error, "Expected an error but did not get one");
@@ -85,6 +92,17 @@ contract('Remittance -Given puzzle set by alice', (accounts) => {
         assert.equal(log.args.sender, carol);
         assert.equal(log.args.amount.toString(10), web3.utils.toWei('2', 'ether').toString(10));
     });
+
+    it('Should allow alice reclaim the funds before expiration', async () => {
+        try{
+            await instance.payerReclaimFundsAfterExpirationDate({from: alice});
+            throw null;
+        } catch(error) {
+            assert.isNotNull(error, "Expected an error but did not get one");
+            assert.include(error.message, "revert");
+            assert.include(error.message, "not expired");
+        }
+    });
 });
 
 contract('Remittance -Given puzzle set by alice and solved by carol', (accounts) => {
@@ -95,13 +113,13 @@ contract('Remittance -Given puzzle set by alice and solved by carol', (accounts)
     beforeEach('set up contract with puzzle and funds from alice then solve by carol', async () => {
         instance = await Remittance.new({from: owner});
         puzzle = await instance.generatePuzzle(secret1, secret2);
-        await instance.setupPuzzleAndFunds(puzzle, {from: alice, value: web3.utils.toWei('2', 'ether')});
+        await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020, {from: alice, value: web3.utils.toWei('2', 'ether')});
         await instance.solvePuzzleAndClaimFunds(text, text2, {from: carol});
     });
 
     it('Should not allow alice to reset puzzle', async () => {
         try{
-            await instance.setupPuzzleAndFunds(puzzle, {from: alice, value: web3.utils.toWei('2', 'ether')});
+            await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020, {from: alice, value: web3.utils.toWei('2', 'ether')});
             throw null;
         } catch(error) {
             assert.isNotNull(error, "Expected an error but did not get one");
@@ -113,6 +131,73 @@ contract('Remittance -Given puzzle set by alice and solved by carol', (accounts)
     it ('Should not allow Carol to solve puzzle', async () => {
         try{
             await instance.solvePuzzleAndClaimFunds(text, text, {from: carol});
+            throw null;
+        } catch(error) {
+            assert.isNotNull(error, "Expected an error but did not get one");
+            assert.include(error.message, "revert");
+            assert.include(error.message, "no funds");
+        }
+    });
+
+    it('Should allow alice reclaim the funds', async () => {
+        try{
+            await instance.payerReclaimFundsAfterExpirationDate({from: alice});
+            throw null;
+        } catch(error) {
+            assert.isNotNull(error, "Expected an error but did not get one");
+            assert.include(error.message, "revert");
+            assert.include(error.message, "not expired");
+        }
+    });
+});
+
+contract('Remittance - Given expiration date has expired', (accounts) => {
+    const owner = accounts[0]
+    const alice = accounts[4];
+    const carol = accounts[6];
+
+    beforeEach('set up contract with puzzle and funds from alice', async() => {
+        instance = await Remittance.new({from: owner});
+        puzzle = await instance.generatePuzzle(secret1, secret2);
+        await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020, {from: alice, value: web3.utils.toWei('2', 'ether')});
+        let snapshot = await tm.takeSnapshot();
+        snapshotId = snapshot.result;
+    });
+
+    afterEach(async() => {
+        await tm.revertToSnapshot(snapshotId);
+    });
+
+    it ('Should allow alice to reclaim funds after expiration date', async () => {
+        await tm.advanceBlockAndSetTime(Fri_25_Sep_00_00_00_BST_2020);
+        tx = await instance.payerReclaimFundsAfterExpirationDate({from: alice});
+        const { logs } = tx;
+        const log = logs[0];
+        assert.equal(log.event, 'LogPayerReclaimsFunds');
+        assert.equal(log.args.sender, alice);
+        assert.equal(log.args.amount.toString(10), web3.utils.toWei('2', 'ether').toString(10));
+        // It should then not allow carol to solve the puzzle.
+        try{
+            await instance.solvePuzzleAndClaimFunds(text, text, {from: carol});
+            throw null;
+        } catch(error) {
+            assert.isNotNull(error, "Expected an error but did not get one");
+            assert.include(error.message, "revert");
+            assert.include(error.message, "no funds");
+        }
+    });
+
+    it ('Should not allow Carol to solve puzzle after expiration date', async () => {
+        await tm.advanceBlockAndSetTime(Fri_25_Sep_00_00_00_BST_2020);
+        tx = await instance.solvePuzzleAndClaimFunds(text, text2, {from: carol});
+        const { logs } = tx;
+        const log = logs[0];
+        assert.equal(log.event, 'LogClaimFunds');
+        assert.equal(log.args.sender, carol);
+        assert.equal(log.args.amount.toString(10), web3.utils.toWei('2', 'ether').toString(10));
+        // It should then not allow alice to reclaim the funds.
+        try{
+            await instance.payerReclaimFundsAfterExpirationDate({from: alice});
             throw null;
         } catch(error) {
             assert.isNotNull(error, "Expected an error but did not get one");
