fc6a4b7de2fe6ce8373ce4cf98537071e14a1149
mikeoconnor <1531855+mikeoconnor@users.noreply.github.com>
2020-05-15 18:28:12 +0100

    Refactor unit tests so they use package ganache-assertions

--- test/remittance.test.js
+++ test/remittance.test.js
@@ -1,5 +1,6 @@
 const Remittance = artifacts.require("Remittance");
 const tm = require('ganache-time-traveler');
+const truffleAssert = require('truffle-assertions');
 
 let instance = null;
 let tx = null;
@@ -8,6 +9,7 @@ const secret1 = "onetime1";
 const secret2 = "onetime2";
 const text = "onetime1";
 const text2 = "onetime2";
+const expectedAmount = "2000000000000000000";
 
 // The expiration date
 const Tue_15_Sep_00_00_00_BST_2020 = 1600124400;
@@ -26,25 +28,20 @@ contract('Remittance - Given new contract', (accounts) => {
 
     it('should allow alice to set puzzle with funds', async () => {
         puzzle = await instance.generatePuzzle(secret1, secret2);
-        tx = await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020, {from: alice, value: web3.utils.toWei('2', 'ether')});
-        const { logs } = tx;
-        const log = logs[0];
-        assert.equal(log.event, 'LogSetup');
-        assert.equal(log.args.sender, alice);
-        assert.equal(log.args.amount.toString(10), web3.utils.toWei('2', 'ether').toString(10));
-        assert.equal(log.args.puzzle, puzzle);
+        tx = await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020,
+                {from: alice, value: web3.utils.toWei('2', 'ether')});
+        truffleAssert.eventEmitted(tx, 'LogSetup', (ev) => {
+             return  ev.sender === alice && ev.amount.toString(10) === expectedAmount &&
+                     ev.puzzle !== 0 &&
+                     ev.expirationDate.toString(10) === Tue_15_Sep_00_00_00_BST_2020.toString(10);
+        });
     });
 
     it ('Should not allow Carol to solve puzzle', async () => {
-        try{
-            const text = "onetime1";
-            await instance.solvePuzzleAndClaimFunds(text, text, {from: carol});
-            throw null;
-        } catch(error) {
-            assert.isNotNull(error, "Expected an error but did not get one");
-            assert.include(error.message, "revert");
-            assert.include(error.message, "no puzzle");
-        }
+        await truffleAssert.reverts(
+            instance.solvePuzzleAndClaimFunds(text, text, {from: carol}),
+            "no puzzle"
+        );
     });
 });
 
@@ -56,52 +53,37 @@ contract('Remittance -Given puzzle set by alice', (accounts) => {
     beforeEach('set up contract with puzzle and funds from alice', async () => {
         instance = await Remittance.new({from: owner});
         puzzle = await instance.generatePuzzle(secret1, secret2);
-        await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020, {from: alice, value: web3.utils.toWei('2', 'ether')});
+        await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020,
+            {from: alice, value: web3.utils.toWei('2', 'ether')});
     });
 
     it('Should not allow alice to reset puzzle', async () => {
-        try{
-            await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020, {from: alice, value: web3.utils.toWei('2', 'ether')});
-            throw null;
-        } catch(error) {
-            assert.isNotNull(error, "Expected an error but did not get one");
-            assert.include(error.message, "revert");
-            assert.include(error.message, "puzzle should be zero");
-        }
+        await truffleAssert.reverts(
+            instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020,
+                {from: alice, value: web3.utils.toWei('2', 'ether')}),
+            "puzzle should be zero"
+        );
     });
 
     it('Should not allow carol to solve puzzle with incorrect data', async () => {
-        try{
-            const text = "onetime1";
-            await instance.solvePuzzleAndClaimFunds(text, text, {from: carol});
-            throw null;
-        } catch(error) {
-            assert.isNotNull(error, "Expected an error but did not get one");
-            assert.include(error.message, "revert");
-            assert.include(error.message, "Puzzle not solved");
-        }
+        await truffleAssert.reverts(
+            instance.solvePuzzleAndClaimFunds(text, text, {from: carol}),
+            "Puzzle not solved"
+        );
     });
 
     it('Should allow carol to solve puzzle and collect funds', async () => {
-        const text = "onetime1";
-        const text2 = "onetime2";
         tx = await instance.solvePuzzleAndClaimFunds(text, text2, {from: carol});
-        const { logs } = tx;
-        const log = logs[0];
-        assert.equal(log.event, 'LogClaimFunds');
-        assert.equal(log.args.sender, carol);
-        assert.equal(log.args.amount.toString(10), web3.utils.toWei('2', 'ether').toString(10));
+        truffleAssert.eventEmitted(tx, 'LogClaimFunds', (ev) => {
+            return ev.sender === carol && ev.amount.toString(10) === expectedAmount;
+        });
     });
 
     it('Should allow alice reclaim the funds before expiration', async () => {
-        try{
-            await instance.payerReclaimFundsAfterExpirationDate({from: alice});
-            throw null;
-        } catch(error) {
-            assert.isNotNull(error, "Expected an error but did not get one");
-            assert.include(error.message, "revert");
-            assert.include(error.message, "not expired");
-        }
+        await truffleAssert.reverts(
+            instance.payerReclaimFundsAfterExpirationDate({from: alice}),
+            "not expired"
+        );
     });
 });
 
@@ -113,41 +95,31 @@ contract('Remittance -Given puzzle set by alice and solved by carol', (accounts)
     beforeEach('set up contract with puzzle and funds from alice then solve by carol', async () => {
         instance = await Remittance.new({from: owner});
         puzzle = await instance.generatePuzzle(secret1, secret2);
-        await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020, {from: alice, value: web3.utils.toWei('2', 'ether')});
+        await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020,
+            {from: alice, value: web3.utils.toWei('2', 'ether')});
         await instance.solvePuzzleAndClaimFunds(text, text2, {from: carol});
     });
 
     it('Should not allow alice to reset puzzle', async () => {
-        try{
-            await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020, {from: alice, value: web3.utils.toWei('2', 'ether')});
-            throw null;
-        } catch(error) {
-            assert.isNotNull(error, "Expected an error but did not get one");
-            assert.include(error.message, "revert");
-            assert.include(error.message, "puzzle should be zero");
-        }
+        await truffleAssert.reverts(
+            instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020,
+                {from: alice, value: web3.utils.toWei('2', 'ether')}),
+            "puzzle should be zero"
+        );
     });
 
     it ('Should not allow Carol to solve puzzle', async () => {
-        try{
-            await instance.solvePuzzleAndClaimFunds(text, text, {from: carol});
-            throw null;
-        } catch(error) {
-            assert.isNotNull(error, "Expected an error but did not get one");
-            assert.include(error.message, "revert");
-            assert.include(error.message, "no funds");
-        }
+        await truffleAssert.reverts(
+            instance.solvePuzzleAndClaimFunds(text, text, {from: carol}),
+            "no funds"
+        );
     });
 
     it('Should allow alice reclaim the funds', async () => {
-        try{
-            await instance.payerReclaimFundsAfterExpirationDate({from: alice});
-            throw null;
-        } catch(error) {
-            assert.isNotNull(error, "Expected an error but did not get one");
-            assert.include(error.message, "revert");
-            assert.include(error.message, "not expired");
-        }
+        await truffleAssert.reverts(
+            instance.payerReclaimFundsAfterExpirationDate({from: alice}),
+            "not expired"
+        );
     });
 });
 
@@ -159,7 +131,8 @@ contract('Remittance - Given expiration date has expired', (accounts) => {
     beforeEach('set up contract with puzzle and funds from alice', async() => {
         instance = await Remittance.new({from: owner});
         puzzle = await instance.generatePuzzle(secret1, secret2);
-        await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020, {from: alice, value: web3.utils.toWei('2', 'ether')});
+        await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020,
+            {from: alice, value: web3.utils.toWei('2', 'ether')});
         let snapshot = await tm.takeSnapshot();
         snapshotId = snapshot.result;
     });
@@ -168,42 +141,30 @@ contract('Remittance - Given expiration date has expired', (accounts) => {
         await tm.revertToSnapshot(snapshotId);
     });
 
-    it ('Should allow alice to reclaim funds after expiration date', async () => {
+    it ('Should allow alice to reclaim funds after expiration date (if not solved by carol)', async () => {
         await tm.advanceBlockAndSetTime(Fri_25_Sep_00_00_00_BST_2020);
         tx = await instance.payerReclaimFundsAfterExpirationDate({from: alice});
-        const { logs } = tx;
-        const log = logs[0];
-        assert.equal(log.event, 'LogPayerReclaimsFunds');
-        assert.equal(log.args.sender, alice);
-        assert.equal(log.args.amount.toString(10), web3.utils.toWei('2', 'ether').toString(10));
+        truffleAssert.eventEmitted(tx, 'LogPayerReclaimsFunds', (ev) => {
+            return ev.sender === alice && ev.amount.toString(10) === expectedAmount;
+        });
         // It should then not allow carol to solve the puzzle.
-        try{
-            await instance.solvePuzzleAndClaimFunds(text, text, {from: carol});
-            throw null;
-        } catch(error) {
-            assert.isNotNull(error, "Expected an error but did not get one");
-            assert.include(error.message, "revert");
-            assert.include(error.message, "no funds");
-        }
+        await truffleAssert.reverts(
+            instance.solvePuzzleAndClaimFunds(text, text, {from: carol}),
+            "no funds"
+        );
     });
 
-    it ('Should not allow Carol to solve puzzle after expiration date', async () => {
+    it ('Should allow Carol to solve puzzle after expiration date (if not reclaimed by alice)', async () => {
         await tm.advanceBlockAndSetTime(Fri_25_Sep_00_00_00_BST_2020);
         tx = await instance.solvePuzzleAndClaimFunds(text, text2, {from: carol});
-        const { logs } = tx;
-        const log = logs[0];
-        assert.equal(log.event, 'LogClaimFunds');
-        assert.equal(log.args.sender, carol);
-        assert.equal(log.args.amount.toString(10), web3.utils.toWei('2', 'ether').toString(10));
+        truffleAssert.eventEmitted(tx, 'LogClaimFunds', (ev) => {
+            return ev.sender === carol && ev.amount.toString(10) === expectedAmount;
+        });
         // It should then not allow alice to reclaim the funds.
-        try{
-            await instance.payerReclaimFundsAfterExpirationDate({from: alice});
-            throw null;
-        } catch(error) {
-            assert.isNotNull(error, "Expected an error but did not get one");
-            assert.include(error.message, "revert");
-            assert.include(error.message, "no funds");
-        }
+        await truffleAssert.reverts(
+            instance.payerReclaimFundsAfterExpirationDate({from: alice}),
+            "no funds"
+        );
     });
 });
 
