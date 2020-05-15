bcd8f8ad03d048126c161d8893a8f2f0ba190476
mikeoconnor <1531855+mikeoconnor@users.noreply.github.com>
2020-05-15 13:45:12 +0100

    Add expiration date to Remittance
    When setting up the puzzle an expiration date is set. After the
    expiration date the payer may reclaim the funds if the puzzle remains
    unsolved.

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -8,17 +8,23 @@ contract Remittance is Stoppable {
 
     bytes32 private puzzle;
     uint256 funds;
+    address payer;
+    uint256 public expirationDate;
     
-    event LogSetup(address indexed sender, uint256 amount, bytes32 puzzle);
+    event LogSetup(address indexed sender, uint256 amount, bytes32 puzzle, uint256 expirationDate);
     event LogClaimFunds(address indexed sender, uint256 amount);
+    event LogPayerReclaimsFunds(address indexed sender, uint256 amount);
     
-    function setupPuzzleAndFunds(bytes32 _puzzle) public payable ifAlive ifRunning returns(bool success){
+    function setupPuzzleAndFunds(bytes32 _puzzle, uint256 _expirationDate) public payable ifAlive ifRunning returns(bool success){
         require(_puzzle != 0, "No puzzle provided");
+        require(_expirationDate >= now, "expiration date not in future");
         require(puzzle == 0, "puzzle should be zero");
         require(msg.value != 0, "No funds provided");
         puzzle = _puzzle;
+        payer = msg.sender;
+        expirationDate = _expirationDate;
         funds = funds.add(msg.value);
-        emit LogSetup(msg.sender, msg.value, _puzzle);
+        emit LogSetup(msg.sender, msg.value, _puzzle, _expirationDate);
         return true;
     }
     
@@ -36,5 +42,17 @@ contract Remittance is Stoppable {
 
     function generatePuzzle(string memory solution1, string memory solution2) public view returns (bytes32 newPuzzle) {
         return keccak256(abi.encode(address(this), solution1, solution2));
+    }
+
+    function payerReclaimFundsAfterExpirationDate() public payable returns (bool success){
+        require(payer == msg.sender, "not payer");
+        require(now >= expirationDate, "not expired");
+        require(funds != 0, "no funds");
+        uint256 amount = funds;
+        funds = 0;
+        emit LogPayerReclaimsFunds(msg.sender, amount);
+        (bool result, ) = msg.sender.call.value(amount)("");
+        require(result, "Failed to transfer funds");
+        return result;
     }
 }
