5cac1ce378ba0edc29af79bc3503f692ed745269
mikeoconnor <1531855+mikeoconnor@users.noreply.github.com>
2020-05-17 19:35:31 +0100

    Add new unit tests for multiple remittances set by alice

--- test/remittance.test.js
+++ test/remittance.test.js
@@ -7,6 +7,8 @@ let tx = null;
 let puzzle = null;
 const secret1 = "onetime1";
 const secret2 = "onetime2";
+const secret3 = "big";
+const secret4 = "boy";
 const text = "onetime1";
 const text2 = "onetime2";
 const expectedAmount = "2000000000000000000";
@@ -188,6 +190,69 @@ contract('Remittance - Given expiration date has expired', (accounts) => {
             instance.payerReclaimFundsAfterExpirationDate(secret1, secret2, {from: alice}),
             "no funds"
         );
+    });
+});
+
+contract('Remittance - Given new contract', (accounts) => {
+    const owner = accounts[0]
+    const alice = accounts[4];
+    const carol = accounts[6];
+
+    before('set up default contract', async () => {
+        instance = await Remittance.new({from: owner});
+    });
+
+    it('should allow alice to set puzzle with funds', async () => {
+        puzzle = await instance.generatePuzzle(secret1, secret2);
+        tx = await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020,
+                {from: alice, value: web3.utils.toWei('2', 'ether')});
+        truffleAssert.eventEmitted(tx, 'LogSetup', (ev) => {
+             return  ev.sender === alice && ev.amount.toString(10) === expectedAmount &&
+                     ev.puzzle !== 0 &&
+                     ev.expirationDate.toString(10) === Tue_15_Sep_00_00_00_BST_2020.toString(10);
+        });
+    });
+
+    it ('Should then allow alice to set another puzzle with funds', async () => {
+        puzzle = await instance.generatePuzzle(secret3, secret4);
+        tx = await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020,
+                {from: alice, value: web3.utils.toWei('2', 'ether')});
+        truffleAssert.eventEmitted(tx, 'LogSetup', (ev) => {
+             return  ev.sender === alice && ev.amount.toString(10) === expectedAmount &&
+                     ev.puzzle !== 0 &&
+                     ev.expirationDate.toString(10) === Tue_15_Sep_00_00_00_BST_2020.toString(10);
+        });
+    });
+});
+
+contract('Remittance - Given two puzzles set by alice', (accounts) => {
+    const owner = accounts[0]
+    const alice = accounts[4];
+    const bob = accounts[5];
+    const carol = accounts[6];
+
+    before('set up contract with two sets of puzzles and funds from alice', async () => {
+        instance = await Remittance.new({from: owner});
+        puzzle = await instance.generatePuzzle(secret1, secret2);
+        await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020,
+            {from: alice, value: web3.utils.toWei('2', 'ether')});
+        puzzle = await instance.generatePuzzle(secret3, secret4);
+        await instance.setupPuzzleAndFunds(puzzle, Tue_15_Sep_00_00_00_BST_2020,
+            {from: alice, value: web3.utils.toWei('2', 'ether')});
+    });
+
+    it('Should allow carol to solve puzzle and collect funds', async () => {
+        tx = await instance.solvePuzzleAndClaimFunds(text, text2, {from: carol});
+        truffleAssert.eventEmitted(tx, 'LogClaimFunds', (ev) => {
+            return ev.sender === carol && ev.amount.toString(10) === expectedAmount;
+        });
+    });
+
+    it('Should then allow bob to solve puzzle and collect funds', async () => {
+        tx = await instance.solvePuzzleAndClaimFunds("big", "boy", {from: bob});
+        truffleAssert.eventEmitted(tx, 'LogClaimFunds', (ev) => {
+            return ev.sender === bob && ev.amount.toString(10) === expectedAmount;
+        });
     });
 });
 
