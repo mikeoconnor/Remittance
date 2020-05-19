7df53827405690def716366d6daebd0f7c13ac78
mikeoconnor <1531855+mikeoconnor@users.noreply.github.com>
2020-05-19 19:39:27 +0100

    Add shop owner's address to PaymentStruct of Remittance
    PaymentStruct.shop; the shop owner's address is used to make sure that
    only the shop owner can solve the puzzle and claim funds.
    funcion generatePuzzle(address shop, string memory solution2) has been
    modified so it now takes the shop owner's address as first parameter.
    function setupPuzzleAndFunds(bytes32 _puzzle, address _shop, uint256
    _expirationDate) has been modified so that the shop owner's address is
    required (to generate puzzle and to set payments[puzzle].shop.
    Summary:
    When shop owner carol solves puzzle, only shop owner's address and bob's
    one-time-secret password are required.

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -8,6 +8,7 @@ contract Remittance is Stoppable {
 
     struct PaymentStruct {
         address payer;
+        address shop;
         uint256 funds;
         uint256 expirationDate;
     }
@@ -18,21 +19,28 @@ contract Remittance is Stoppable {
     event LogClaimFunds(address indexed sender, uint256 amount);
     event LogPayerReclaimsFunds(address indexed sender, uint256 amount);
     
-    function setupPuzzleAndFunds(bytes32 _puzzle, uint256 _expirationDate) public payable ifAlive ifRunning returns(bool success){
+    function setupPuzzleAndFunds(bytes32 _puzzle, address _shop, uint256 _expirationDate) public payable ifAlive ifRunning returns(bool success){
         require(_puzzle != 0, "No puzzle provided");
+        require(_shop != address(0), "no shop");
         require(_expirationDate >= now, "expiration date not in future");
         require(msg.value != 0, "No funds provided");
         require(payments[_puzzle].payer == address(0), "puzzle should be zero");
+        require(payments[_puzzle].shop == address(0), "puzzle should be zero");
         payments[_puzzle].payer = msg.sender;
+        payments[_puzzle].shop = _shop;
         payments[_puzzle].expirationDate = _expirationDate;
         payments[_puzzle].funds = msg.value;
         emit LogSetup(msg.sender, msg.value, _puzzle, _expirationDate);
         return true;
     }
     
-    function solvePuzzleAndClaimFunds(string memory solution1, string memory solution2) public payable ifAlive ifRunning returns(bool success){
-        bytes32 puzzle = generatePuzzle(solution1, solution2);
+    function solvePuzzleAndClaimFunds(string memory solution2) public payable ifAlive ifRunning returns(bool success){
+        address _shop = msg.sender;
+        require(_shop != address(0), "no shop");
+        bytes32 puzzle = generatePuzzle(_shop, solution2);
         require(payments[puzzle].payer != address(0), "Puzzle not solved");
+        require(payments[puzzle].shop != address(0), "Puzzle not solved");
+        require(payments[puzzle].shop == _shop, "not shop");
         require(payments[puzzle].funds != 0, "no funds");
         uint256 amount = payments[puzzle].funds;
         payments[puzzle].funds = 0;
@@ -42,12 +50,12 @@ contract Remittance is Stoppable {
         return result;
     }
 
-    function generatePuzzle(string memory solution1, string memory solution2) public view returns (bytes32 newPuzzle) {
-        return keccak256(abi.encode(address(this), solution1, solution2));
+    function generatePuzzle(address shop, string memory solution2) public view returns (bytes32 newPuzzle) {
+        return keccak256(abi.encode(address(this), shop, solution2));
     }
 
-    function payerReclaimFundsAfterExpirationDate(string memory solution1, string memory solution2) public payable returns (bool success){
-        bytes32 puzzle = generatePuzzle(solution1, solution2);
+    function payerReclaimFundsAfterExpirationDate(address _shop, string memory solution2) public payable returns (bool success){
+        bytes32 puzzle = generatePuzzle(_shop, solution2);
         require(payments[puzzle].payer != address(0), "Puzzle not solved");
         require(payments[puzzle].payer == msg.sender, "not payer");
         require(now >= payments[puzzle].expirationDate, "not expired");
