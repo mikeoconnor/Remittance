caf62ca1b67b12dbd37b7cee56da6b0990e417fa
mikeoconnor <1531855+mikeoconnor@users.noreply.github.com>
2020-05-24 18:25:31 +0100

    Tidy UI

--- app/js/app.js
+++ app/js/app.js
@@ -15,25 +15,10 @@ Remittance.setProvider(web3.currentProvider);
 
 window.addEventListener('load', async() => {
     try {
-        const accounts = await web3.eth.getAccounts();
-        window.owner = accounts[0];
-        window.alice = accounts[1];
-        window.carol = accounts[2];
-
-        console.log("Owner", window.owner);
-        console.log("Alice", window.alice);
-        console.log("Carol", window.carol);
-
-        const network = await web3.eth.net.getId();
-        console.log("Network", network.toString(10));
-
         const inst = await Remittance.deployed();
-        console.log("Contract Address", inst.address);
         $("#address").html("Contract Address: " + inst.address);
         $("#status").html("Status: OK.");
-
         $("#send").click(setup);
-
         $("#claim").click(solve);
     } catch (e) {
         console.log("error", e);
@@ -43,20 +28,12 @@ window.addEventListener('load', async() => {
 
 const setup = async () => {
     try {
-        console.log("function setup entered...");
-        console.log("shop: ", $("input[name='shop']").val());
-        console.log("password ", $("input[name='password']").val());
-
         const inst = await Remittance.deployed();
         const puzzle = await inst.generatePuzzle($("input[name='shop']").val(), $("input[name='password']").val());
-        console.log("puzzle ", puzzle);
 
         //Expiration date is hard fixed to be 30 days from now.
         const DAYS30 = 30*86400;
         const expiration = Math.floor(Date.now() / 1000) + DAYS30;
-        console.log("Expiration date ", expiration);
-        console.log("Amount: ", $("input[name='amount']").val());
-        console.log("Payer ", $("input[name='payer']").val());
 
         try{
             await inst.setupPuzzleAndFunds.call(
@@ -90,7 +67,7 @@ const setup = async () => {
             $("#status").html("Status: There was an error in the tx execution, missing logs");
         } else {
             console.log(receipt.logs[0]);
-            $("#status").html("Status: Remittance transaction executed");
+            $("#status").html("Status: Remittance created");
         }
 
     } catch (e){
@@ -101,14 +78,7 @@ const setup = async () => {
 
 const solve = async () => {
     try {
-        console.log("function solve entered...");
-        console.log("shop: ", $("input[name='shop2']").val());
-        console.log("password ", $("input[name='password2']").val());
-
         const inst = await Remittance.deployed();
-
-        const puzzle = await inst.generatePuzzle($("input[name='shop2']").val(), $("input[name='password2']").val());
-        console.log("puzzle ", puzzle);
 
         try{
             await inst.solvePuzzleAndClaimFunds.call(
@@ -139,7 +109,7 @@ const solve = async () => {
             $("#status2").html("Status: There was an error in the tx execution, missing logs");
         } else {
             console.log(receipt.logs[0]);
-            $("#status2").html("Status: Remittance transaction executed");
+            $("#status2").html("Status: Remittance claimed");
         }
     } catch (e) {
         $("#status2").html("Status: " + e.toString());
